a5a2f1f33d9dbac09751255c0ff9830137f51afa
Muhammad Taimoor Khalid <157301054+mtaimoorkhalid@users.noreply.github.com>
2025-12-25 20:26:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// These lines mimic "__dirname" which doesn't exist in Module mode
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Serve the 'dist' folder (where React builds to)
 app.use(express.static(path.join(__dirname, 'dist')));
 
+// Fix the "Refresh" 404 error
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port: ${port}`);
+  console.log(`Server running on port ${port}`);
 });
